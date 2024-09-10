3b3039535398259989733f071b74ffec62bab118
sandeepThapaMagar510 <sandeepthapa520@gmail.com>
2024-09-10 22:54:51 +0545

    day 1 of learning react

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Sandeep Thapa Magar
+
         </a>
       </header>
     </div>
