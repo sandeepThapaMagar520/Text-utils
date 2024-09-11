df5219ff524dae5c06020c702628f9c83c477420
sandeepThapaMagar510 <sandeepthapa520@gmail.com>
2024-09-12 01:13:51 +0545

    day 3 started

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,28 +4,50 @@ import React, {useState} from 'react'
 
 
 export default function ContactForm(props) {
+  // changing text to upper case 
   const handleUppercase=()=>{
-      console.log("uppercase button was clicked " + text);
       let newText = text.toUpperCase();
       setText(newText)
-
+  }
+  // changing text to lower case
+  const handleLowercase=()=>{
+    let newText = text.toLowerCase();
+    setText(newText)
+  }
+  //clearing 
+  const handleBlank=()=>{
+    let newText = "";
+    setText(newText)
   }
 
   const handleonchange=(event)=>{
-    console.log("handleonchange button was clicked ");
-    setText(event.target.value);
-    
+    setText(event.target.value);  
+  }
 
-}
-  const [text,setText] = useState("Enter text here!")
+  const [text,setText] = useState("")
 
   return (
-    <div>
-        <h1>{props.heading} </h1>
-        <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleonchange} id="myBox" rows="8"></textarea>
-        </div>
-        <button className="btn btn-primary" onClick={handleUppercase}>Convert to Uppercase</button>
-    </div>
+    <>
+      <div className='container'>
+          <h1>{props.heading} </h1>
+          <div className="mb-3">
+              <textarea className="form-control" value={text} onChange={handleonchange} id="myBox" rows="8" placeholder='Enter your text here'></textarea>
+          </div>
+          <button className="btn btn-primary mx-2" onClick={handleUppercase}>Convert to Uppercase</button>
+          <button className="btn btn-primary mx-2" onClick={handleLowercase}>Convert to Lowercase</button>
+          <button className="btn btn-primary mx-2" onClick={handleBlank}>Clear</button>
+
+      </div>
+      <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p>Your Text Word: <b>{text.split(/\s+/).length}</b></p>         
+        <p>Your Text characters:<b> {text.length}</b> </p>
+        <p>Minutes to read: <b>{0.008* (text.split(" ").length)}</b></p>
+
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
+    </>
+    
   )
 }
