c9c8ee091fc1adeb599f369fd52482d39b9f33e8
sandeepThapaMagar510 <sandeepthapa520@gmail.com>
2024-09-12 23:19:39 +0545

    added alert messages

--- src/App.js
+++ src/App.js
@@ -2,26 +2,44 @@ import './App.css';
 import Navbar from './components/Navbar'; 
 import ContactForm from './components/ContactForm'; 
 import { useState } from 'react';
+import Alert from './components/Alert';
 // import About from "./components/About";
 
 function App() {
   const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null)
+
+  const showAlert =(message, type) =>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=>{
+      setAlert(null)
+    },1500)
+
+  }
+
+  // to change mode (dark and light mode)
   const togglemode =()=>{
     if(mode ==="light"){
       setMode("dark");
-      document.body.style.backgroundColor="grey";
+      document.body.style.backgroundColor="#042743";
+      showAlert("Dark mode has been enabled", "success");
     }
     else{
       setMode("light");
       document.body.style.backgroundColor="white";
+      showAlert("Light mode has been enabled", "success");
     }
   }
   return (
     <>
       <Navbar title="TextUtils" mode = {mode} toggleMode={togglemode}/>
+      <Alert alert={alert}/>
       <div className="container my-3">
          
-        <ContactForm heading="Enter the text to analyze below" mode={mode} />
+        <ContactForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert}/>
         {/* <About/> */}
     
       </div>
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,16 +8,20 @@ export default function ContactForm(props) {
   const handleUppercase=()=>{
       let newText = text.toUpperCase();
       setText(newText)
+      props.showAlert("Converted to upper case", "success")
+
   }
   // changing text to lower case
   const handleLowercase=()=>{
     let newText = text.toLowerCase();
     setText(newText)
+    props.showAlert("Converted to lower case", "success")
   }
   //clearing 
   const handleBlank=()=>{
     let newText = "";
     setText(newText)
+    props.showAlert("Deleted", "danger")
   }
 
   const handleonchange=(event)=>{
@@ -28,20 +32,23 @@ export default function ContactForm(props) {
     let text = document.getElementById("myBox");
     text.select();
     navigator.clipboard.writeText(text.value);
+    props.showAlert("Text Copied", "success")
   }
   const handleExtraSpaces =() =>{
     let removedSpaces = text.split(/[ ]+/);
     setText(removedSpaces.join(" "))
+    props.showAlert("Spaces removed", "success")
   }
 
   const [text,setText] = useState("")
 
   return (
     <>
-      <div className='container'>
-          <h1>{props.heading} </h1>
+      <div className='container' style={{color: props.mode==="dark"?"white":"black"}}>
+          <h1 >{props.heading} </h1>
           <div className="mb-3">
-              <textarea className="form-control" value={text} onChange={handleonchange} id="myBox" rows="8" placeholder='Enter your text here' style={{backgroundColor: props.mode==="dark"?"grey":"white"}}></textarea>
+              <textarea className="form-control" value={text} onChange={handleonchange} id="myBox" rows="8" placeholder='Enter your text here' style={{backgroundColor: props.mode==="dark"?"#042743":"white",color: props.mode==="dark"?"white":"black"}}>
+              </textarea>
           </div>
           <button className="btn btn-primary mx-2" onClick={handleUppercase}>Convert to Uppercase</button>
           <button className="btn btn-primary mx-2" onClick={handleLowercase}>Convert to Lowercase</button>
@@ -50,14 +57,14 @@ export default function ContactForm(props) {
           <button className="btn btn-primary mx-2"onClick={handleExtraSpaces}>Clear Spaces</button>
 
       </div>
-      <div className="container my-3">
+      <div className="container my-3" style={{color: props.mode==="dark"?"white":"black"}}>
         <h2>Your text summary</h2>
         <p>Your Text Word: <b>{text.split(/\s+/).length}</b></p>         
         <p>Your Text characters:<b> {text.length}</b> </p>
         <p>Minutes to read: <b>{0.008* (text.split(" ").length)}</b></p>
 
         <h3>Preview</h3>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter something in the textbox to preview here!"}</p>
       </div>
     </>
     
